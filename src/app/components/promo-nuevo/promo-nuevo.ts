7ae38802fd8f9c5f430b8ae4988c525b71c0be13
Jesus Abraham Robledo Lopez <abrahamrobledo@MacBook-Air-de-Abraham.local>
2025-10-16 15:24:14 +0200

    Agregados nuevos productos imagenes

--- src/app/components/promo-nuevo/promo-nuevo.ts
+++ src/app/components/promo-nuevo/promo-nuevo.ts
@@ -11,26 +11,40 @@ import { CommonModule } from '@angular/common';
 export class PromoNuevo implements OnInit {
   @ViewChild('carrusel', { static: false }) carrusel!: ElementRef<HTMLDivElement>;
 
+
   imagenes = [
-    { src: 'https://photo.yupoo.com/2022cap/0ae6373a/d9470387.png', alt: 'Producto 1' },
-    { src: 'https://photo.yupoo.com/2022cap/f0b95b37/71831e81.png', alt: 'Producto 2' },
-    { src: 'https://photo.yupoo.com/2022cap/403e9d66/024a924f.png', alt: 'Producto 3' },
-    { src: 'https://photo.yupoo.com/2022cap/92d4e6de/759a2fde.png', alt: 'Producto 4' },
-    { src: 'https://photo.yupoo.com/2022cap/faf6130e/6ce6b55e.png', alt: 'Producto 5' },
-    { src: 'https://photo.yupoo.com/2022cap/66843493/b5b6163c.png', alt: 'Producto 6' },
-    { src: 'https://photo.yupoo.com/2022cap/606fb6ef/04e2675a.png', alt: 'Producto 7' },
-    { src: 'https://photo.yupoo.com/2022cap/e3abf634/011067bc.png', alt: 'Producto 8' },
-    { src: 'https://photo.yupoo.com/2022cap/a713aebb/8f00637e.jpeg', alt: 'Producto 9' },
-    { src: 'https://photo.yupoo.com/2022cap/dd48af75/e05ff874.png', alt: 'Producto 10' },
-    { src: 'https://photo.yupoo.com/2022cap/7adfbc73/ceff9887.png', alt: 'Producto 11' },
-    { src: 'https://photo.yupoo.com/2022cap/e38c4671/4ba41d16.png', alt: 'Producto 12' },
-    { src: 'https://photo.yupoo.com/2022cap/969b41ac/9c788b91.png', alt: 'Producto 13' },
-    { src: 'https://photo.yupoo.com/2022cap/a2d439e2/dff97087.png', alt: 'Producto 14' },
-    { src: 'https://photo.yupoo.com/2022cap/6728580a/60dc9d53.png', alt: 'Producto 15' },
-    { src: 'https://photo.yupoo.com/2022cap/22cf9262/6039c92a.png', alt: 'Producto 16' },
-    { src: 'https://drop-shop.mx/cdn/shop/files/DSC05663_1400x_71a9f234-b959-44ac-a423-1114c6f4386d.webp?v=1730743555', alt: 'Producto 17' },
-    { src: 'https://photo.yupoo.com/2022cap/6657a42d/a0768247.jpeg', alt: 'Producto 18' }
-  ];
+  { src: './promos/1.png', alt: 'Producto 1' },
+  { src: './promos/2.png', alt: 'Producto 2' },
+  { src: './promos/3.png', alt: 'Producto 3' },
+  { src: './promos/4.png', alt: 'Producto 4' },
+  { src: './promos/5.jpeg', alt: 'Producto 5' },
+  { src: './promos/6.png', alt: 'Producto 6' },
+  { src: './promos/7.png', alt: 'Producto 7' },
+  { src: './promos/8.png', alt: 'Producto 8' },
+  { src: './promos/9.png', alt: 'Producto 9' },
+  { src: './promos/10.png', alt: 'Producto 10' },
+  { src: './promos/11.png', alt: 'Producto 11' },
+  { src: './promos/12.png', alt: 'Producto 12' },
+  { src: './promos/13.png', alt: 'Producto 13' },
+  { src: './promos/14.png', alt: 'Producto 14' },
+  { src: './promos/15.png', alt: 'Producto 15' },
+  { src: './promos/16.jpeg', alt: 'Producto 16' },
+  { src: './promos/17.webp', alt: 'Producto 17' },
+  { src: './promos/18.webp', alt: 'Producto 18' },
+  { src: './promos/19.jpg', alt: 'Producto 19' },
+  { src: './promos/20.webp', alt: 'Producto 20' },
+  { src: './promos/21.webp', alt: 'Producto 21' },
+  { src: './promos/22.jpg', alt: 'Producto 22' },
+  { src: './promos/23.webp', alt: 'Producto 23' },
+  { src: './promos/24.webp', alt: 'Producto 24' },
+  { src: './promos/25.jpg', alt: 'Producto 25' },
+  { src: './promos/26.jpeg', alt: 'Producto 26' },
+  { src: './promos/27.webp', alt: 'Producto 27' },
+  { src: './promos/28.webp', alt: 'Producto 28' },
+  { src: './promos/29.jpeg', alt: 'Producto 29' },
+  { src: './promos/30.jpeg', alt: 'Producto 30' }
+];
+
 
   index = 0;
 
